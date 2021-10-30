c9e71111c32bfc0a1d2ff25fe7089656e7c1a220
thamika-git <adheesha@raditha.com>
2021-10-30 12:45:26 +0530

    Make the colors look a bit nicer using HSL.

--- src/index.jsx
+++ src/index.jsx
@@ -3,18 +3,18 @@ import ReactDOM from "react-dom";
 
 import Card from "./components/Card";
 
-function random() {
-    return Math.floor(Math.random() * 255);
+function random(max) {
+    return Math.floor(Math.random() * max);
 }
 
 function App() {
     const colors = Array.from({ length: 4 }, () => {
-        return `rgb(${random()}, ${random()}, ${random()})`;
+        return `hsl(${random(360)}, 80%, 70%)`;
     });
 
     return (
         <div>
-            <h1 className="text-center mt-32 md:mt-48 lg:mt-60 text-3xl font-sans font-black text-gray-700">
+            <h1 className="text-center mt-32 md:mt-48 text-3xl font-sans font-black text-gray-700">
                 Color palette generator
             </h1>
             <div className="flex flex-row flex-wrap justify-center mt-16">
