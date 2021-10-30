61c7b2b9f771b8ee126050fb6f9f314fbe8bcad6
thamika-git <adheesha@raditha.com>
2021-10-30 12:12:38 +0530

    Generate palette randomly.

--- src/index.jsx
+++ src/index.jsx
@@ -3,17 +3,24 @@ import ReactDOM from "react-dom";
 
 import Card from "./components/Card";
 
+function random() {
+    return Math.floor(Math.random() * 255);
+}
+
 function App() {
+    const colors = Array.from({ length: 4 }, () => {
+        return `rgb(${random()}, ${random()}, ${random()})`;
+    });
+
     return (
         <div>
             <h1 className="text-center mt-32 md:mt-48 lg:mt-60 text-3xl font-sans font-black text-gray-700">
                 Color palette generator
             </h1>
             <div className="flex flex-row flex-wrap justify-center mt-16">
-                <Card color="#60A5FA" />
-                <Card color="#34D399" />
-                <Card color="#FBBF24" />
-                <Card color="#F87171" />
+                {colors.map(color => {
+                    return <Card key={color} color={color} />;
+                })}
             </div>
         </div>
     );
